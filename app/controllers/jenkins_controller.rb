7ee6a1df2a219462b85ecdee618c05c8b04dfe40
Michael Hayes <tasslehawf@mac.com>
2013-10-14 15:28:42 -0500

    jenkins controller updated with Bundle.with_clean_env block

--- app/controllers/jenkins_controller.rb
+++ app/controllers/jenkins_controller.rb
@@ -17,29 +17,31 @@ class JenkinsController < MVCLI::Controller
     tmpdir = Pathname(Dir.tmpdir).join 'chef_kitchen'
     FileUtils.mkdir_p tmpdir
     Dir.chdir tmpdir do
-      File.open('Gemfile', 'w') do |f|
-        f.puts 'source "https://rubygems.org"'
-        f.puts 'gem "knife-solo", ">= 0.3.0pre3"'
-        f.puts 'gem "berkshelf"'
-      end
-      Bundler.with_clean_env { execute "bundle install --binstubs" }
-      execute "bin/knife solo init ."
-      FileUtils.rm "Berksfile"
-      File.open 'Berksfile', 'w' do |f|
-        f.puts "site :opscode"
-        f.puts ""
-        f.puts "cookbook 'runit', '>= 1.1.2'"
-        f.puts "cookbook 'jenkinsbox', github: 'hayesmp/jenkins-rackbox-cookbook'"
-      end
-      execute "bin/berks install --path cookbooks/"
-      execute "bin/knife solo prepare root@#{server.ipv4_address}"
-      File.open('nodes/host.json', 'w') do |f|
-        f.puts("{\"run_list\":[\"recipe[build-essential]\",\"recipe[jenkinsbox::postgresql]\",\"recipe[jenkinsbox]\",\"recipe[jenkinsbox::jenkins]\"],\"jenkinsbox\":{\"build_essential\":{ \"compiletime\":true},\"jenkins\":{\"git_name\":\"#{form.git_name}\",\"git_email\":\"#{form.git_email}\",\"ip_address\":\"#{server.ipv4_address}\", \"host\":\"#{server.name}\"},\"ruby\":{\"versions\":[\"2.0.0-p247\"],\"global_version\":\"2.0.0-p247\"},\"apps\":{\"unicorn\":[{\"appname\":\"app1\",\"hostname\":\"app1\"}]},\"db_root_password\":\"iloverandompasswordsbutthiswilldo\", \"databases\":{\"postgresql\":[{\"database_name\":\"app1_production\",\"username\":\"app1\",\"password\":\"app1_pass\"}]}}}")
-      end
+      Bundler.with_clean_env do
+        File.open('Gemfile', 'w') do |f|
+          f.puts 'source "https://rubygems.org"'
+          f.puts 'gem "knife-solo", ">= 0.3.0pre3"'
+          f.puts 'gem "berkshelf"'
+        end
+        execute "bundle install --binstubs"
+        execute "bin/knife solo init ."
+        FileUtils.rm "Berksfile"
+        File.open 'Berksfile', 'w' do |f|
+          f.puts "site :opscode"
+          f.puts ""
+          f.puts "cookbook 'runit', '>= 1.1.2'"
+          f.puts "cookbook 'jenkinsbox', github: 'hayesmp/jenkins-rackbox-cookbook'"
+        end
+        execute "bin/berks install --path cookbooks/"
+        execute "bin/knife solo prepare root@#{server.ipv4_address}"
+        File.open('nodes/host.json', 'w') do |f|
+          f.puts("{\"run_list\":[\"recipe[build-essential]\",\"recipe[jenkinsbox::postgresql]\",\"recipe[jenkinsbox]\",\"recipe[jenkinsbox::jenkins]\"],\"jenkinsbox\":{\"build_essential\":{ \"compiletime\":true},\"jenkins\":{\"git_name\":\"#{form.git_name}\",\"git_email\":\"#{form.git_email}\",\"ip_address\":\"#{server.ipv4_address}\", \"host\":\"#{server.name}\"},\"ruby\":{\"versions\":[\"2.0.0-p247\"],\"global_version\":\"2.0.0-p247\"},\"apps\":{\"unicorn\":[{\"appname\":\"app1\",\"hostname\":\"app1\"}]},\"db_root_password\":\"iloverandompasswordsbutthiswilldo\", \"databases\":{\"postgresql\":[{\"database_name\":\"app1_production\",\"username\":\"app1\",\"password\":\"app1_pass\"}]}}}")
+        end
 
-      FileUtils.rm_rf "#{server.ipv4_address}.json"
-      FileUtils.mv "nodes/host.json", "nodes/#{server.ipv4_address}.json"
-      execute "bin/knife solo cook root@#{server.ipv4_address} -V"
+        FileUtils.rm_rf "#{server.ipv4_address}.json"
+        FileUtils.mv "nodes/host.json", "nodes/#{server.ipv4_address}.json"
+        execute "bin/knife solo cook root@#{server.ipv4_address} -V"
+      end
     end
     return server
   end
