711ec2d54324bcf565d880d0755aea3907dd0a34
Michael Hayes <tasslehawf@mac.com>
2013-11-05 16:52:44 -0600

    new create application code using usergrid_iron gem

--- app/controllers/usergridapps_controller.rb
+++ app/controllers/usergridapps_controller.rb
@@ -16,27 +16,16 @@ class UsergridappsController < MVCLI::Controller
     management = Usergrid::Management.new "http://#{server.ipv4_address}:8080"
     management.login form.admin_username, form.admin_password
     organization = management.organization form.org_name
-    application = organization.create_application form.app_name
-
-    uri = URI.parse("http://#{server.ipv4_address}:8080/management/orgs/#{form.org_name}/apps")
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri, {'Authorization' => 'Bearer #{form.auth_token}', 'Content-Type' => 'application/json'})
-    request.set_form({"name" => form.app_name})
-    command.output.puts uri.request_uri
-    response = http.request(request)
-    command.output.puts response.body.inspect
-    if response.code == "200"
+    #fail organization.response.inspect
+    begin
+      organization.create_application form.app_name
+      #command.output.puts application.inspect
       command.output.puts "Organization name: #{form.org_name}"
       command.output.puts "Application name: #{form.app_name}"
-      command.output.puts "Management auth token: #{form.auth_token}"
-
-      login_admin
-    elsif response.code == "400"
-      command.output.puts "Application already exists."
-      login_admin
-    else
-      command.output.puts "Something went wrong: create application. Response code: #{response.code}"
+    rescue
+      command.output.puts "Something went wrong. Most likely the application already exists."
     end
+    #fail application.response.inspect
     return server
   end
 
