5193ac7f90d58e0916c30fb005a714f9c16e16dd
Michael Hayes <tasslehawf@mac.com>
2013-11-05 15:19:49 -0600

    create organization no longer logs in admin user

--- app/controllers/usergridorgs_controller.rb
+++ app/controllers/usergridorgs_controller.rb
@@ -19,40 +19,21 @@ class UsergridorgsController < MVCLI::Controller
                             "password" => @form.admin_password})
     response = http.request(request)
     #command.output.puts response.body.inspect
-    if response.code == "200"
-      command.output.puts "Organization: #{@form.org_name}"
-      command.output.puts "Admin Name: #{@form.admin_name}"
-      command.output.puts "Admin Username: #{@form.admin_username}"
-      command.output.puts "Admin Email: #{@form.admin_email}"
-      command.output.puts "Admin Password: #{@form.admin_password}"
+    command.output.puts "Organization: #{@form.org_name}"
+    command.output.puts "Admin Name: #{@form.admin_name}"
+    command.output.puts "Admin Username: #{@form.admin_username}"
+    command.output.puts "Admin Email: #{@form.admin_email}"
+    command.output.puts "Admin Password: #{@form.admin_password}"
 
-      login_admin
-    elsif response.code == "400"
-      command.output.puts "Organization already exists. Logging in."
-      login_admin
+    if response.code == "400"
+      command.output.puts "Organization or Andministrator already exists."
     else
-      command.output.puts "Something went wrong: create organization. Response code: #{response.code}"
+      command.output.puts "Something went wrong:#{JSON.parse(response.body)['error']}  Response code: #{response.code}"
     end
     return server
   end
 
   private
-
-  def login_admin
-    uri = URI.parse("http://#{server.ipv4_address}:8080/management/token")
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.set_form_data({"grant_type" => "password", "username" => @form.admin_username,
-                            "password" => @form.admin_password})
-    response = http.request(request)
-    #command.output.puts response.body.inspect
-    parsed = JSON.parse(response.body)
-    if response.code == "200"
-      command.output.puts "Admin User auth token: #{parsed['access_token']}"
-    else
-      command.output.puts "Something went wrong: login admin user. Response code: #{response.code}"
-    end
-  end
 
   def server
     compute.servers.find {|s| s.name == params[:id]} or fail Fog::Errors::NotFound
--- app/forms/usergridorgs/create_form.rb
+++ app/forms/usergridorgs/create_form.rb
@@ -1,10 +1,10 @@
 class Usergridorgs::CreateForm < MVCLI::Form
   requires :naming
 
-  input :org_name, String, default: 'My Organization'
-  input :admin_username, String, default: 'admin'
-  input :admin_name, String, default: 'Admin'
-  input :admin_email, String, default: 'admin@usergrid.com'
+  input :org_name, String, default: 'my_organization'
+  input :admin_username, String, default:'my_org_admin'
+  input :admin_name, String, default: 'My Org Admin'
+  input :admin_email, String, default: 'my_org_admin@usergrid.com'
   input :admin_password, String, default: 'admin_pass'
 
 end
