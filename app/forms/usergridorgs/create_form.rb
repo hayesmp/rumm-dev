710dbc8d53fec722bfa6ecab1e43741f5d4cd2d9
Michael Hayes <tasslehawf@mac.com>
2013-11-13 12:23:15 -0600

    Fix some usergrid typos

--- app/forms/usergridorgs/create_form.rb
+++ app/forms/usergridorgs/create_form.rb
@@ -3,7 +3,7 @@ class Usergridorgs::CreateForm < MVCLI::Form
 
   input :org_name, String, default: 'my_organization'
   input :admin_username, String, default:'my_org_admin'
-  input :admin_name, String, default: 'My Org Admin'
+  input :admin_name, String, default: 'MyOrgAdmin'
   input :admin_email, String, default: 'my_org_admin@usergrid.com'
   input :admin_password, String, default: 'admin_pass'
 
